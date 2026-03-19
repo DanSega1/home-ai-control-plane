611af642686962abcf16a33ae3459686944deb2b
dans <dans@curve.tech>
2026-03-19 21:12:19 +0200

    feat(engine): ✨ Add memory components to the engine's public API

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,16 @@
 """Conductor Engine foundation package."""
 
 from engine.loader import load_builtin_capabilities, load_capabilities
+from engine.memory import MemoryDocument, MemoryHit, MemoryProvider, MemoryQuery, MemUProvider
 from engine.supervisor.service import TaskSupervisor
 
-__all__ = ["TaskSupervisor", "load_builtin_capabilities", "load_capabilities"]
+__all__ = [
+    "MemUProvider",
+    "MemoryDocument",
+    "MemoryHit",
+    "MemoryProvider",
+    "MemoryQuery",
+    "TaskSupervisor",
+    "load_builtin_capabilities",
+    "load_capabilities",
+]
